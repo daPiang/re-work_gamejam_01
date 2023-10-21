87680b1a12dc71ce18d18ec4c203f8dc774bdc94
Datu Anuarudin P. Piang II <88268514+daPiang@users.noreply.github.com>
2023-10-22 00:12:28 +0800

    dialogue improvements

--- rework_gamjam+_01/Assets/Scripts/DialogueSystem.cs
+++ rework_gamjam+_01/Assets/Scripts/DialogueSystem.cs
@@ -11,9 +11,10 @@ public class DialogueSystem : MonoBehaviour
     [SerializeField] TextMeshProUGUI textComponent;
     [SerializeField] TextMeshProUGUI speakerText;
     [SerializeField] Image playerImage;
-    [SerializeField] Image npcImage;
+    // [SerializeField] Image npcImage;
     public GameObject dialogueStuff;
-    [SerializeField] DialogueLine[] dialogueLines;
+    // [SerializeField] DialogueLine[] dialogueLines;
+    [SerializeField] Dialogues dialogues;
     public float textSpeed;
     public Vector3 playerImageScale = new Vector3(1.2f, 1.2f, 1.2f);
     public Vector3 npcImageScale = new Vector3(1.2f, 1.2f, 1.2f);
@@ -36,16 +37,16 @@ public class DialogueSystem : MonoBehaviour
         textComponent.text = string.Empty;
         speakerText.text = "";
         originalPlayerScale = playerImage.rectTransform.localScale; // Store the original scale of the player image.
-        originalNPCScale = npcImage.rectTransform.localScale; // Store the original scale of the NPC image.
+        originalNPCScale = dialogues.speakerImage.rectTransform.localScale; // Store the original scale of the NPC image.
         // StartDialogue();
     }
 
     public void StartDialogue()
     {
         index = 0;
-        StartCoroutine(TypeLine(dialogueLines[index].text));
-        EmphasizeSpeaker(dialogueLines[index].speaker);
-        UpdateSpeakerNameUI(dialogueLines[index].speaker);
+        StartCoroutine(TypeLine(dialogues.dialogueLines[index].text));
+        EmphasizeSpeaker(dialogues.dialogueLines[index].speaker);
+        UpdateSpeakerNameUI(dialogues.dialogueLines[index].speaker);
     }
 
     IEnumerator TypeLine(string line)
@@ -62,35 +63,35 @@ public class DialogueSystem : MonoBehaviour
         if (speaker == SpeakerType.Player)
         {
             playerImage.rectTransform.localScale = playerImageScale;
-            npcImage.rectTransform.localScale = originalNPCScale; // Reset the NPC image scale.
+            dialogues.speakerImage.rectTransform.localScale = originalNPCScale; // Reset the NPC image scale.
         }
         else if (speaker == SpeakerType.NPC)
         {
-            npcImage.rectTransform.localScale = npcImageScale;
+            dialogues.speakerImage.rectTransform.localScale = npcImageScale;
             playerImage.rectTransform.localScale = originalPlayerScale; // Reset the player image scale.
         }
         else
         {
             playerImage.rectTransform.localScale = originalPlayerScale;
-            npcImage.rectTransform.localScale = originalNPCScale;
+            dialogues.speakerImage.rectTransform.localScale = originalNPCScale;
         }
     }
 
     void UpdateSpeakerNameUI(SpeakerType speaker)
     {
-        string speakerName = (speaker == SpeakerType.Player) ? "Player" : "NPC";
+        string speakerName = (speaker == SpeakerType.Player) ? "Player" : dialogues.speakerName;
         speakerText.text = speakerName;
     }
 
     void NextLine()
     {
-        if (index < dialogueLines.Length - 1)
+        if (index < dialogues.dialogueLines.Length - 1)
         {
             index++;
             textComponent.text = string.Empty;
-            StartCoroutine(TypeLine(dialogueLines[index].text));
-            EmphasizeSpeaker(dialogueLines[index].speaker);
-            UpdateSpeakerNameUI(dialogueLines[index].speaker);
+            StartCoroutine(TypeLine(dialogues.dialogueLines[index].text));
+            EmphasizeSpeaker(dialogues.dialogueLines[index].speaker);
+            UpdateSpeakerNameUI(dialogues.dialogueLines[index].speaker);
         }
         else
         {
@@ -106,14 +107,14 @@ public class DialogueSystem : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (textComponent.text == dialogueLines[index].text)
+            if (textComponent.text == dialogues.dialogueLines[index].text)
             {
                 NextLine();
             }
             else
             {
                 StopAllCoroutines();
-                textComponent.text = dialogueLines[index].text;
+                textComponent.text = dialogues.dialogueLines[index].text;
             }
         }
     }
