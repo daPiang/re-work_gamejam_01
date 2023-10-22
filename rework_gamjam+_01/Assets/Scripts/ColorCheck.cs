77ccaae28c10403432b9ec8a30a3f89deefe0367
Datu Anuarudin P. Piang II <88268514+daPiang@users.noreply.github.com>
2023-10-22 20:53:16 +0800

    Revert "stage 2-3 drawables"
    This reverts commit 923718d21807268a3f5f7ef02a94fab2dc20e193.

--- rework_gamjam+_01/Assets/Scripts/ColorCheck.cs
+++ rework_gamjam+_01/Assets/Scripts/ColorCheck.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Destructible2D;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class ColorCheck : MonoBehaviour
 {
@@ -10,23 +9,8 @@ public class ColorCheck : MonoBehaviour
     [Range(0f, 1f)]
     public float threshold;
 
-    [SerializeField] private Light2D light;
-    [SerializeField] private ParticleSystem particles;
-
     [SerializeField] private Brush.ColorState colorState;
     public string clearsFlag;
-
-    private void Start() {
-        if(particles != null)
-        {
-            particles.Stop();
-        }
-
-        if(light != null)
-        {
-            light.enabled = false;
-        }
-    }
 
     private void Update() {
         if((float)desprite.AlphaCount/desprite.OriginalAlphaCount <= threshold)
@@ -35,16 +19,6 @@ public class ColorCheck : MonoBehaviour
             // desprite.AlphaCount = 0;
             GetComponent<SpriteRenderer>().enabled = false;
             FlagSystem.instance.SetFlag(clearsFlag, true);
-
-            if(particles != null)
-            {
-                particles.Play();
-            }
-
-            if(light != null)
-            {
-                light.enabled = true;
-            }
         }
 
         // Debug.Log(ColorMatch());
