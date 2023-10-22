2fa7bcf287133b440e4966369679afc3fd7d4120
Earl Batenga <61070797+ErlRuben@users.noreply.github.com>
2023-10-22 16:07:54 +0800

    TransitionFix

--- rework_gamjam+_01/Assets/Scripts/SceneHandler.cs
+++ rework_gamjam+_01/Assets/Scripts/SceneHandler.cs
@@ -38,10 +38,42 @@ public class SceneHandler: MonoBehaviour
             isClicked = false;
         }
     }
-    IEnumerator LoadLevel(){
+    IEnumerator LoadLevel()
+    {
+        // Trigger the "FadeOut" animation
         transition.SetTrigger("FadeOut");
-        yield return new WaitForSeconds(1f);
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex+1);
-        transition.SetTrigger("FadeIn");
+
+        // Wait for the animation to finish
+        yield return new WaitForSeconds(transition.GetCurrentAnimatorClipInfo(0)[0].clip.length);
+
+        // Determine the next scene index
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        // Check if the next scene index is valid
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            // Load the next scene additively
+            AsyncOperation loadOperation = SceneManager.LoadSceneAsync(nextSceneIndex, LoadSceneMode.Additive);
+
+            // Wait until the next scene is fully loaded
+            while (!loadOperation.isDone)
+            {
+                yield return null;
+            }
+
+            // Unload the current scene (optional)
+            SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+
+            // Set the newly loaded scene as the active scene
+            SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(nextSceneIndex));
+
+            // Trigger the "FadeIn" animation (you might want to add a delay if needed)
+            transition.SetTrigger("FadeIn");
+        }
+        else
+        {
+            Debug.LogWarning("No more scenes to load.");
+        }
     }
+
 }
